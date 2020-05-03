5cde941663dc0832956ab6e88cd4f504147b888d
fir0j <firoj.is.available@gmail.com>
2020-05-03 20:32:47 +0530

    Updates

---
+++ precache-manifest.9b1c1dccf444ca51dfc7b2bfa75cf766.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cea68b83bfc9226adcab3ff9a70567",
+    "url": "/react-context-shopping-cart/index.html"
+  },
+  {
+    "revision": "25403f840ca7f6c0c5e2",
+    "url": "/react-context-shopping-cart/static/js/2.2e00842f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-context-shopping-cart/static/js/2.2e00842f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570fae6fd3fc8d31aa67",
+    "url": "/react-context-shopping-cart/static/js/main.f872caf4.chunk.js"
+  },
+  {
+    "revision": "ba26455b0f4d83294b46",
+    "url": "/react-context-shopping-cart/static/js/runtime-main.f35274fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Multiple-Shopping-Cart-Prototypes/precache-manifest.a434835cf98bc8e66a39163708ae378e.js"
+  "/react-context-shopping-cart/precache-manifest.9b1c1dccf444ca51dfc7b2bfa75cf766.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Multiple-Shopping-Cart-Prototypes/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-context-shopping-cart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
